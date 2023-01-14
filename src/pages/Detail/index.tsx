392125a3cc651d608979ca0c7e76ce995ee310b0
leondavidtb <leondavidtb@gmail.com>
2023-01-14 17:22:25 -0300

    feat: created Pokemon Detail page

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,19 +1,131 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRoute, useNavigation } from "@react-navigation/native";
-import { Text } from "react-native";
+import { Alert, ScrollView, Text } from "react-native";
+import api from "../../service/api";
+import { useTheme } from "styled-components";
+import { Feather } from "@expo/vector-icons";
+import { circle } from "../../assets/img/circle.png";
+import { dots } from "../../assets/img/dots.png";
+import * as Styled from "./styles";
 
 type RouteParams = {
   pokemonId: number;
 };
 
+type Stats = {
+  base_stat: 62;
+  stat: {
+    name: string;
+  };
+};
+
+type Abilities = {
+  ability: {
+    name: string;
+  };
+};
+
+export type PokemonTypeName =
+  | "grass"
+  | "fire"
+  | "water"
+  | "poison"
+  | "normal"
+  | "bug"
+  | "flying"
+  | "eletric"
+  | "ground";
+
+type PokemonType = {
+  type: {
+    name: PokemonTypeName;
+  };
+};
+
+type PokemonProps = {
+  id: number;
+  name: string;
+  color: string;
+  stats: Stats[];
+  abilities: Abilities[];
+  types: PokemonType[];
+};
+
 export function Detail() {
   const route = useRoute();
-
   const { pokemonId } = route.params as RouteParams;
+  const { colors } = useTheme();
+  const [pokemonDetails, setPokemonDetail] = useState({} as PokemonProps);
+  const [isLoading, setIsLoading] = useState(true);
+  const { goBack } = useNavigation();
+
+  function handleGoBack() {
+    goBack();
+  }
+
+  useEffect(() => {
+    async function getPokemonDetail() {
+      try {
+        const response = await api.get(`/pokemon/${pokemonId}`);
+        const { stats, abilities, id, name, types } = response.data;
+
+        const currentType = types[0].type.name as PokemonTypeName;
+        const color = colors.backgroundCard[currentType];
+
+        setPokemonDetail({
+          stats,
+          abilities,
+          id,
+          name,
+          types,
+          color,
+        });
+      } catch (error) {
+        Alert.alert("Oops, there was an error!");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getPokemonDetail();
+  }, []);
 
   return (
     <>
-      <Text style={{ margin: 100 }}>{pokemonId}</Text>
+      {isLoading ? (
+        <Text style={{ marginTop: 200 }}>Carregando...</Text>
+      ) : (
+        <ScrollView style={{ flex: 1, backgroundColor: "#fff" }}>
+          <Styled.Header type={pokemonDetails.types[0].type.name}>
+            <Styled.BackButton onPress={handleGoBack}>
+              <Feather name="chevron-left" size={24} color="#fff" />
+            </Styled.BackButton>
+
+            <Styled.ContentImage>
+              <Styled.CircleImage source={circle} />
+              <Styled.PokemonImage
+                source={{
+                  uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonDetails.id}.png`,
+                }}
+              />
+            </Styled.ContentImage>
+            <Styled.Content>
+              <Styled.PokemonId>#{pokemonDetails.id}</Styled.PokemonId>
+              <Styled.PokemonName>{pokemonDetails.name}</Styled.PokemonName>
+              <Styled.PokemonTypeContainer>
+                {pokemonDetails.types.map(({ type }) => (
+                  <Styled.PokemonType key={type.name} type={type.name}>
+                    <Styled.PokemonTypeText>{type.name}</Styled.PokemonTypeText>
+                  </Styled.PokemonType>
+                ))}
+              </Styled.PokemonTypeContainer>
+            </Styled.Content>
+            <Styled.DotsImage source={dots} />
+          </Styled.Header>
+
+          <Styled.Container></Styled.Container>
+        </ScrollView>
+      )}
     </>
   );
 }
---
+++ src/pages/Detail/styles.ts
@@ -0,0 +1,110 @@
+import styled, { css } from "styled-components/native";
+import { PokemonTypeName } from ".";
+
+type TypeProps = {
+  type: PokemonTypeName;
+};
+
+export const Header = styled.View<TypeProps>`
+  ${({ theme, type }) => css`
+    background-color: ${theme.colors.backgroundCard[type]};
+    height: 340px;
+    padding: 20px;
+    flex-direction: row;
+    align-items: center;
+    position: relative;
+  `}
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 70px;
+  left: 40px;
+`;
+
+export const ContentImage = styled.View`
+  position: relative;
+`;
+
+export const CircleImage = styled.Image`
+  width: 125px;
+  height: 125px;
+  position: absolute;
+`;
+
+export const PokemonImage = styled.Image`
+  width: 125px;
+  height: 125px;
+`;
+
+export const Content = styled.View`
+  margin-left: 30px;
+`;
+
+export const PokemonId = styled.Text`
+  ${({ theme }) => css`
+    font-size: 16px;
+    line-height: 19px;
+    font-style: normal;
+    font-weight: bold;
+    color: ${theme.colors.light_text};
+  `}
+`;
+
+export const PokemonName = styled.Text`
+  ${({ theme }) => css`
+    text-transform: capitalize;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 28px;
+
+    line-height: 38px;
+    color: ${theme.colors.text_white};
+  `}
+`;
+
+export const PokemonTypeContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const PokemonType = styled.View<TypeProps>`
+  ${({ theme, type }) => css`
+    width: 61px;
+    height: 25px;
+
+    background: ${theme.colors.boxType[type]};
+    border-radius: 3px;
+    justify-content: center;
+    align-items: center;
+    margin-left: 5px;
+    margin-top: 10px;
+  `}
+`;
+
+export const PokemonTypeText = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.text_white};
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    font-style: normal;
+  `}
+`;
+
+export const DotsImage = styled.Image`
+  width: 85px;
+  position: absolute;
+  right: -20px;
+  top: 220px;
+`;
+
+export const Container = styled.View`
+  ${({ theme }) => css`
+    flex: 1;
+    padding: 20px;
+    background-color: ${theme.colors.background};
+    border-top-right-radius: 30px;
+    border-top-left-radius: 30px;
+    margin-top: -40px;
+  `}
+`;
